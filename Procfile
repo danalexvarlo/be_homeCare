d947d56837821998e19d4d5e1df852223aad296e
dannyvargaslo <danalexvarlo@gmail.com>
2022-09-23 15:10:24 -0500

    cambios

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn homeCare.wsgi
--- homeCareApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomecareappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'homeCareApp'
